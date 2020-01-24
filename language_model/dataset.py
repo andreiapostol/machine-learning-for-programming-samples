ff63ccc56f842f63fd781afde988ff85558a86bc
Andrei-Laurentiu Apostol <andreiapostol@dhcp-10-248-115-242.eduroam.wireless.private.cam.ac.uk>
2020-01-24 12:18:26 +0000

    First commit

--- language_model/dataset.py
+++ language_model/dataset.py
@@ -6,6 +6,8 @@ import numpy as np
 from more_itertools import chunked
 from dpu_utils.mlutils.vocabulary import Vocabulary
 
+from graph_pb2 import Graph
+from graph_pb2 import FeatureNode
 
 DATA_FILE_EXTENSION = "proto"
 START_SYMBOL = "%START%"
@@ -36,7 +38,76 @@ def load_data_file(file_path: str) -> Iterable[List[str]]:
         Iterable of lists of strings, each a list of tokens observed in the data.
     """
     #TODO 2# Insert your data parsing code here
-    return TODO
+    # Method for checking if a node is a method
+    def isMethod(node):
+        return node.type == FeatureNode.AST_ELEMENT and node.contents == "METHOD"
+
+    # Method that decides whether a node is a token
+    def isToken(node):
+        return node.type in (FeatureNode.TOKEN, FeatureNode.IDENTIFIER_TOKEN)
+
+    # Retrieve token leaf nodes, by DFS
+    def get_leaf_nodes(nodeId, sourceDict, nodeDict, visited):
+        if (nodeId in visited):
+            return []
+        visited.add(nodeId)
+        if (nodeId == None or nodeDict.get(nodeId) == None):
+            return []
+        if (nodeDict.get(nodeId).type in [FeatureNode.TOKEN, FeatureNode.IDENTIFIER_TOKEN]):
+            return [nodeDict.get(nodeId)]
+        edgeTo = sourceDict.get(nodeId)
+        if (edgeTo == None):
+            return []
+        to_return = []
+        for edge in edgeTo:
+            to_return += get_leaf_nodes(edge.destinationId, sourceDict, nodeDict, visited)
+        return to_return
+
+    # Reorder leaf nodes from top to bottom
+    def reorder_leaves(leaves_arr, sourceDict, nodeDict):
+        leaves_map = dict()
+        for (index, node) in enumerate(leaves_arr):
+            leaves_map[node.id] = index
+        length = len(leaves_arr)
+        index_sum = int(((length - 1) * length) / 2)
+        for node in leaves_arr:
+            if (node.id in sourceDict) and ((sourceDict[node.id][0]).destinationId in leaves_map):
+                index_sum -= leaves_map[(sourceDict[node.id][0]).destinationId]
+        current = leaves_arr[index_sum]
+        to_return = []
+        for _ in range(length):
+            to_return.append(current)
+            if current.id in sourceDict:
+                current = nodeDict[(sourceDict[current.id][0]).destinationId]
+            else:
+                break
+        return to_return
+
+    # Get tokens for given file
+    with open(file_path, "rb") as f:
+        g = Graph()
+        g.ParseFromString(f.read())
+        token_count = len(list(filter(lambda n:n.type in 
+                                (FeatureNode.TOKEN,
+                                FeatureNode.IDENTIFIER_TOKEN), g.node)))
+        to_print_len = min(len(g.node), 100)
+        idsInNode = dict()
+        sourceIdsInEdge = dict()
+        for node in g.node:
+            idsInNode[node.id] = node
+        for edge in g.edge:
+            cur = sourceIdsInEdge.get(edge.sourceId, [])
+            cur.append(edge)
+            sourceIdsInEdge[edge.sourceId] = cur
+        all_results = []
+        for node in g.node:
+            if isMethod(node):
+                initial_leaves = reorder_leaves(get_leaf_nodes(node.id, sourceIdsInEdge, idsInNode, set()), \
+                                                sourceIdsInEdge, idsInNode)
+                correct = [str(n.contents).lower() for n in filter(isToken, initial_leaves)]
+                all_results.append(correct)
+        return all_results
+    # return TODO
 
 
 def build_vocab_from_data_dir(
